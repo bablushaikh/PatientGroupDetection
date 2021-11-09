3a3397eba9a76ee372a8947e4e0225986cc63359
CORP\Shaikh.Bablu <Shaikh.Bablu@neudesic.com>
2021-11-10 00:05:25 +0530

    Added core logic to GetPatiantGroupCount method

--- PatientGroupDetection.Application/Repository/PatientRepository.cs
+++ PatientGroupDetection.Application/Repository/PatientRepository.cs
@@ -8,10 +8,26 @@ namespace PatientGroupDetection.Application.Repository
 {
     public class PatientRepository
     {
-        //public int[][] Matrix { get; set; }
         public int GetPatientGroupCount(int[][] Matrix)
         {
+            
+            for(int i = 0; i<Matrix.Length;i++)
+            {
+                for (int j = 0; j < Matrix.Length; i++)
+                {
+                    Console.WriteLine(Matrix[i][j]);
+                    if(Matrix[i][j]== 1)
+                    {
+
+                    }
+                }
+                    
+            }
             return 0;
+        }
+        private void CheckPatientExistance()
+        {
+
         }
     }
 }
